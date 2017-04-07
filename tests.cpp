82b1f7aa07b2e775e9ebd538cc91e5f4c8822ec2
FantacyWorld <brucehuang0711@qq.com>
2017-04-07 10:38:02 +0800

    test 2 for dyn_array_import

--- tests.cpp
+++ tests.cpp
@@ -9,25 +9,24 @@
 
 const int array_size = 100;
 
-<<<<<<< HEAD
-void destructor_dyn_array_t(void * dyn_arr)
+
+void destructor_dyn_array_t(void * dyn_arr_dp)
 {
-	dyn_array_clear((dyn_array_t *)dyn_arr);
-	free(((dyn_array_t *)dyn_arr)->array);
+    if(dyn_arr_dp)
+    {
+        dyn_array_destroy((dyn_array_t *)*dyn_arr_dp);
+    }
 }
 
-=======
->>>>>>> 8081a490136e26286bd84ceec32aa0557aa1238f
 // 0 capacity
 TEST(dyn_array_create, create_with_zero_capacity)
 {
     size_t data_type_size = 4;
     dyn_array_t * dyn_arr = dyn_array_create(0, data_type_size, NULL);
     EXPECT_NE(nullptr, dyn_arr) << "dyn_array_create failed with 0 capacity" << std::endl;
-<<<<<<< HEAD
+
 	dyn_array_destroy(dyn_arr);
-=======
->>>>>>> 8081a490136e26286bd84ceec32aa0557aa1238f
+
 }
 
 // high capacity
@@ -37,10 +36,8 @@ TEST(dyn_array_create, create_with_high_capacity)
     size_t data_type_size = 4;
     dyn_array_t * dyn_arr = dyn_array_create(high_capacity, data_type_size, NULL);
     EXPECT_NE(nullptr, dyn_arr) << "dyn_array_create failed with a high capacity" << std::endl;
-<<<<<<< HEAD
 	dyn_array_destroy(dyn_arr);
-=======
->>>>>>> 8081a490136e26286bd84ceec32aa0557aa1238f
+
 }
 
 // import from an array with int
@@ -59,25 +56,20 @@ TEST(dyn_array_import, import_array_with_int_type)
 // eg: dyn_array_t
 TEST(dyn_array_import, import_array_with_custom_type)
 {
-<<<<<<< HEAD
-	dyn_array_t * array = (dyn_array_t *)calloc(array_size, sizeof(dyn_array_t));
+    int array[array_size];
+	for(int i = 0;i < array_size;i++) array[i] = i;
+    
+	dyn_array_t * dyn_array_array[array_size];
 
 	for(int i = 0;i < array_size;i++)
 	{
 		dyn_array_t * dyn_arr = dyn_array_import(array, array_size, sizeof(int), NULL);
 		ASSERT_NE(nullptr, dyn_arr) << "dyn_array_import failed with an array with int type" << std::endl;
-		//array[i] = *dyn_arr;
-		memcpy(array + i, dyn_arr, sizeof(dyn_array_t));
-		free(dyn_arr);
+		array[i] = dyn_arr;
 	}
-	dyn_array_t * dyn_arr = dyn_array_import(array, array_size, sizeof(dyn_array_t), destructor_dyn_array_t);
+	dyn_array_t * dyn_arr = dyn_array_import(array, array_size, sizeof(dyn_array_t *), destructor_dyn_array_t);
     EXPECT_NE(nullptr, dyn_arr) << "dyn_array_import failed with an array with dyn_array_t type" << std::endl;
 	dyn_array_destroy(dyn_arr);
-=======
-
-	
-
->>>>>>> 8081a490136e26286bd84ceec32aa0557aa1238f
 }
 
 
